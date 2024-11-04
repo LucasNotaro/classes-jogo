b37c53057a75dc9f0310704c81e6767618473f40
LucasNotaro <lucas.sn@live.com>
2024-11-04 14:58:56 -0300

    adicionado o método atacar e novos heróis

--- index.js
+++ index.js
@@ -4,8 +4,36 @@ class Heroi {
       this.idade = idade;
       this.tipo = tipo;
     }
+  
+    atacar() {
+      let ataque;
+      switch (this.tipo) {
+        case 'mago':
+          ataque = 'magia';
+          break;
+        case 'guerreiro':
+          ataque = 'espada';
+          break;
+        case 'monge':
+          ataque = 'artes marciais';
+          break;
+        case 'ninja':
+          ataque = 'shuriken';
+          break;
+        default:
+          ataque = 'um ataque desconhecido';
+      }
+      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
   }
   
   const mago = new Heroi('Harry', 22, 'mago');
-  console.log(`${mago.nome}, ${mago.idade} anos, é um ${mago.tipo}.`);
+  const guerreiro = new Heroi('Hercules', 39, 'guerreiro');
+  const monge = new Heroi('Shaolin', 40, 'monge');
+  const ninja = new Heroi('Ryu', 41, 'ninja');
+  
+  mago.atacar();
+  guerreiro.atacar();
+  monge.atacar();
+  ninja.atacar();
   
